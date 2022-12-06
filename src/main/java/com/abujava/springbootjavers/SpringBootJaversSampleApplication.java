88127faf1837462f71138d1c346d29f7370b413c
Muhammad <shaymanov.2000@gmail.com>
2022-12-06 11:09:00 +0500

    Bismillah

---
+++ src/main/java/com/abujava/springbootjavers/SpringBootJaversSampleApplication.java
@@ -0,0 +1,13 @@
+package com.abujava.springbootjavers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJaversSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJaversSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abujava/springbootjavers/SpringBootJaversSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abujava.springbootjavers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJaversSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
